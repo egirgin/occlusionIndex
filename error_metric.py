23bd60aaacc6fd92d045c7333b999412e7a721bd
Emre Girgin <emregirgin171@gmail.com>
2023-09-08 19:12:00 +0300

    Add files via upload

---
+++ error_metric.py
@@ -0,0 +1,147 @@
+# Edited from https://github.com/aymenmir1/3dpw-eval/tree/master
+import numpy as np
+
+SMPL_NR_JOINTS = 24
+
+def match_models(preds, gts): # TODO calculate the distance for each pair and match them 
+    for pred in preds:
+        for gt in gts:
+            pass 
+
+def align_models(preds, gts):
+    """
+    If there are more preds then gt, then penalize it 
+    as if there is a gt with all zeros, and vice versa.
+    """
+    
+    num_preds = preds.shape[0]
+    num_gt = gts.shape[0]
+
+    if num_preds != num_gt:
+        max_number = np.max(num_preds, num_gt)
+        
+        preds_new = np.zeros((max_number, 24, 3))
+        preds_new[:num_preds] = preds
+        preds = preds_new
+
+        gts_new = np.zeros((max_number, 24, 3))
+        gts_new[:num_gt] = gts
+        gts = gts_new
+    
+    return preds, gts 
+
+
+def compute_similarity_transform(S1, S2):
+    '''
+    Computes a similarity transform (sR, t) that takes
+    a set of 3D points S1 (3 x N) closest to a set of 3D points S2,
+    where R is an 3x3 rotation matrix, t 3x1 translation, s scale.
+    i.e. solves the orthogonal Procrutes problem.
+    Ensure that the first argument is the prediction
+
+    Source: https://en.wikipedia.org/wiki/Kabsch_algorithm
+
+    :param S1 predicted joint positions array 24 x 3
+    :param S2 ground truth joint positions array 24 x 3
+    :return S1_hat: the predicted joint positions after apply similarity transform
+            R : the rotation matrix computed in procrustes analysis
+    '''
+    # If all the values in pred3d are zero then procrustes analysis produces nan values
+    # Instead we assume the mean of the GT joint positions is the transformed joint value
+
+    if not (np.sum(np.abs(S1)) == 0):
+        transposed = False
+        if S1.shape[0] != 3 and S1.shape[0] != 2:
+            S1 = S1.T
+            S2 = S2.T
+            transposed = True
+        assert (S2.shape[1] == S1.shape[1])
+
+        # 1. Remove mean.
+        mu1 = S1.mean(axis=1, keepdims=True)
+        mu2 = S2.mean(axis=1, keepdims=True)
+        X1 = S1 - mu1
+        X2 = S2 - mu2
+
+        # 2. Compute variance of X1 used for scale.
+        var1 = np.sum(X1 ** 2)
+
+        # 3. The outer producshape1,t of X1 and X2.
+        K = X1.dot(X2.T)
+
+        # 4. Solution that Maximizes trace(R'K) is R=U*V', where U, V are
+        # singular vectors of K.
+        U, s, Vh = np.linalg.svd(K)
+        V = Vh.T
+        # Construct Z that fixes the orientation of R to get det(R)=1.
+        Z = np.eye(U.shape[0])
+        Z[-1, -1] *= np.sign(np.linalg.det(U.dot(V.T)))
+        # Construct R.
+        R = V.dot(Z.dot(U.T))
+
+        # 5. Recover scale.
+        scale = np.trace(R.dot(K)) / var1
+
+        # 6. Recover translation.
+        t = mu2 - scale * (R.dot(mu1))
+
+        # 7. Error:
+        S1_hat = scale * R.dot(S1) + t
+
+        if transposed:
+            S1_hat = S1_hat.T
+
+        return S1_hat, R
+    else:
+        S1_hat = np.tile(np.mean(S2, axis=0), (SMPL_NR_JOINTS, 1))
+        R = np.identity(3)
+
+        return S1_hat, R
+
+def align_by_root(joints):
+    """
+    Assumes joints is 24 x 3 in SMPL order.
+    Subtracts the location of the root joint from all the other joints
+    """
+    root = joints[0, :]
+
+    return joints - root
+
+
+def compute_errors(preds3d, gt3ds):
+    """
+    Gets MPJPE after root alignment + MPJPE after Procrustes.
+    Evaluates on all the 24 joints joints.
+    Inputs:
+    :param gt3ds: N x 24 x 3
+    :param preds: N x 24 x 3
+    :returns
+        MPJPE : scalar - mean of all MPJPE errors
+        MPJPE_PA : scalar- mean of all MPJPE_PA errors
+    """
+
+    preds3d, gt3ds = align_models(preds=preds3d, gt=gt3ds)
+
+    errors, errors_pa = [], []
+
+
+    for i, (gt3d, pred3d) in enumerate(zip(gt3ds, preds3d)):
+        # gt3d = gt3d.reshape(-1, 3)
+        # Root align.
+        gt3d = align_by_root(gt3d)
+        pred3d = align_by_root(pred3d)
+
+        # Compute MPJPE
+        joint_error = np.sqrt(np.sum((gt3d - pred3d) ** 2, axis=1))
+        errors.append(np.mean(joint_error))
+
+        # Compute MPJPE_PA and also store similiarity matrices to apply them later to rotation matrices for MPJAE_PA
+        pred3d_sym, R = compute_similarity_transform(pred3d, gt3d)
+        pa_error = np.sqrt(np.sum((gt3d - pred3d_sym) ** 2, axis=1))
+        errors_pa.append(np.mean(pa_error))
+
+
+    return np.mean(np.array(errors)), np.mean(np.array(errors_pa))
+
+def shape_error(preds, groundtruths): # use maybe rmse for shape errors 
+    pass
